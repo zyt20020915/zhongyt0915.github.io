2a48c12f62db8ab5844f3c131fde41b248b0edd5
masterLijh <maste_rlee.qq.com>
2024-03-08 22:39:23 +0800

    实现了HTML语义建模和满足窄屏渲染的CSS代码，初步完成了响应式设计，下一步尝试用mobile分支开发触屏UI

--- myJs.js
+++ myJs.js
@@ -1,4 +1,4 @@
-  	var deviceWidth = document.body.clientWidth ;
+    var deviceWidth = document.body.clientWidth ;
     var deviceHeight = window.innerHeight ;
     var fontBase = parseInt(deviceWidth / 20);
 	
@@ -8,7 +8,16 @@
     document.body.style.height = deviceHeight + "px" ;
 	document.body.style.fontSize = fontBase + "px" ;
 
-	var lessonNameDom = document.getElementById("lessonName") ;
-	var footerInfoDom = document.getElementById("footerInfo") ;
-	    lessonNameDom.style.lineHeight = deviceHeight * 0.15 + 'px' ;
-        footerInfoDom.style.lineHeight = deviceHeight * 0.1 + 'px' ;
+   //设置下面2行p元素行高与父容器的高度一致，使得文字在垂直方向居中 
+        $("lessonName").style.lineHeight = deviceHeight * 0.15 + 'px' ;
+        $("chapter").style.lineHeight = deviceHeight * 0.1 + 'px' ;
+		$("statusInfo").style.lineHeight = deviceHeight * 0.1 + 'px' ;
+
+  //$(66);检测下面的自定义函数
+  function $(eleId){
+    if (typeof eleId !== 'string'){
+	   throw("$函数调用实参错误，行参必须是字符串！");
+	   return 
+    }
+      return document.getElementById(eleId) ;
+   }
