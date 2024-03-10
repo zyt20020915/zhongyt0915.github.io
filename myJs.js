8570e1a14a9456b2e326d4d0599f8d9c3660694f
masterLijh <maste_rlee.qq.com>
2024-03-10 10:08:34 +0800

    对touch event 的target做了研究，实现了滑动书页画面，切换书本的UI

--- myJs.js
+++ myJs.js
@@ -16,23 +16,31 @@
 //------touch events register and handel----------
 
  const chapterDom = $('chapter') ;
+ const bookPageDom = $('bookPage') ;
        chapterDom.addEventListener("touchstart",handleStart);
 	   chapterDom.addEventListener("touchend",handleEnd);
        chapterDom.addEventListener("touchmove",handleMove);
+	   bookPageDom.addEventListener("touchstart",handleStart);
+	   bookPageDom.addEventListener("touchend",handleEnd);
+       bookPageDom.addEventListener("touchmove",handleMove);
       //---APP开发期间，暂时将底部状态栏设为可无限增加高度的滚动渲染模式。
       $("statusInfo").style.display = "inline" ;
       $("statusInfo").style.overflow = "scroll" ;
+	
 	 function handleStart(e){
+	  touchModel.target = e.touches[0].target ;
 	  e.preventDefault();
 	  const output = $("statusInfo");
-	  const touches = e.changedTouches ;
+	  const touches = e.touches ;
 	  output.textContent = '开始摸时，共有'+touches.length + '个点的数据。';
 	  output.textContent +=  " Touch" + touches[0].identifier +"begin: "  ;
 
 	  //在touch事件发生时用touchMode.time记录开始时刻
       touchModel.time = new Date() - 1 ;
 	  touchModel.ongoingXY = [] ;
+	  
 		 } //function  handleStart
+
      function handleEnd(e){
 	  e.preventDefault();
 	  const output = $("statusInfo");
@@ -44,6 +52,7 @@
 
 	  touchModel.respondTouch() ;
 	 } //function  handleEnd
+
      function handleMove(e){
 	  e.preventDefault();
 	  const output = $("statusInfo");
@@ -63,7 +72,9 @@
 
   //----建立模型响应和处理touch事件产生的数据
  const chapters = ['第1章 Introduction','第2章 Number Systems','第3章 Data Storage','第4章 Computer Organization','第5章 Computer Networks and Internet','第6章 Operating Systems','第7章  Software Engineering','第8章  OOP Programming'] ;
+ const books = ['CS.jpg' , 'CSS.jpg' , 'CT.jpg' , 'GRE.jpg' , 'Git.jpg' , 'NinjaJS.jpg' , 'STEM.jpg' , 'UML.jpg' , 'bitCoin.jpg' , 'canvas.jpg' , 'cssAnimation.jpg' , 'gitForTeams.jpg' , 'internet.jpg' , 'javaScript.jpg' , 'learnCSS.jpg' , 'linuxCMD.jpg' , 'logic.jpg' , 'nutrition.jpg' , 'webProgramming.jpg' ] ;
  var touchModel = {
+   target: null ,
    ongoingXY : [] ,
    deltaX : 0 ,
    deltaY : 0 ,
@@ -73,17 +84,31 @@
      this.ongoingXY.push(xy);
    },
    chapterNo : 0 ,
+   bookNo : 0 ,
    respondTouch : function(){
     this.deltaX = this.ongoingXY[this.ongoingXY.length-1].x - this.ongoingXY[0].x ;
 	this.deltaY = this.ongoingXY[this.ongoingXY.length-1].y - this.ongoingXY[0].y ;
     if (Math.abs(this.deltaX) > deviceWidth / 10) {
-		if (this.deltaX > 0){
-          this.nextChapter();
-		}else{
-		  this.preChapter() ;
+		//console.log("有效滑动");
+		//console.log(this.target) ;
+
+        if (this.target == $('chapter') ){ //touch target is chapters
+		
+    		if (this.deltaX > 0){
+              this.nextChapter();
+		    }else{
+		      this.preChapter() ;
+		    }
+		 }
+		if (this.target == $('bookPage') ){ //touch target is books
+		    if (this.deltaX > 0){
+              this.nextBook();
+		    }else{
+		      this.preBook() ;
+		    }
 		}
     }
-	$("chapter").textContent = chapters[this.chapterNo];
+	
    },
    preChapter : function (){
      if (this.chapterNo ===0)  {
@@ -91,6 +116,7 @@
      }else{
 	     this.chapterNo -- ;
 	 }
+	 $("chapter").textContent = chapters[this.chapterNo];
    },
    nextChapter :function (){
       if (this.chapterNo === chapters.length -1)  {
@@ -98,6 +124,24 @@
      }else{
 	     this.chapterNo ++ ;
 	 }
+	 $("chapter").textContent = chapters[this.chapterNo];
+   },
+   preBook : function(){
+      books
+     if (this.bookNo ===0)  {
+		 this.bookNo = books.length -1 ;
+     }else{
+	     this.bookNo -- ;
+	 }
+	 $("bookPage").src = 'lesson/' + books[this.bookNo];
+   },
+   nextBook :function (){
+      if (this.bookNo === books.length -1)  {
+		 this.bookNo = 0 ;
+     }else{
+	     this.bookNo ++ ;
+	 }
+	  $("bookPage").src = 'lesson/' + books[this.bookNo];
    },
  } ;
 
